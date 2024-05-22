18f2283ed773ea12e5ce4c3c7cd92c884fc61abf
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-05-21 21:25:18 -0600

    Created small blog project with blog route for reading md files

---
+++ minimalistic-blog-next-app-v14-2-3/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import MyProfilePic from "./components/MyProfilePic";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Rigo's Blog",
+    description: "Created by Rigo",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} bg-slate-800`}>
+                <Navbar />
+                <MyProfilePic />
+                {children}
+            </body>
+        </html>
+    );
+}
